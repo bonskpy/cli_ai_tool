f661a9fc0a04ef3806090dad6704e5b729199ad7
bonskpy <bonskpy@gmail.com>
2025-06-20 19:34:14 +0200

    verbose implemented

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ MODEL_ID = "gemini-2.5-flash"  #  ["gemini-2.5-flash", "gemini-2.5-pro", "gemini
 def main():
 
     args: list[str] = sys.argv[1:]
+    verbose: bool = "--verbose" in args
 
     if not args:
         print("\nHello from cli-ai-tool!")
@@ -22,7 +23,7 @@ def main():
         print('     python main.py "How do I build a calculator app?" --verbose\n')
         sys.exit(1)
 
-    user_prompt: str = " ".join(sys.argv[1:])
+    user_prompt: str = " ".join(arg for arg in args if not arg.startswith("--"))
 
     contents: list = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
@@ -30,9 +31,12 @@ def main():
     gem_api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=gem_api_key)
     response = client.models.generate_content(model=MODEL_ID, contents=contents)
+
     if response.usage_metadata and response.text:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if verbose:
+            print(f"User prompt: {user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
         print(f"Response: {response.text.strip()}")
 
 
